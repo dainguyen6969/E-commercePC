06c065a4b3f75230b025c4753e2414a2d5cd42c5
dainguyen6969 <daintth03468@fpt.edu.vn>
2025-12-03 22:35:37 +0700

    feat(security): Implement custom authentication entry point for API.

---
+++ src/main/java/com/dainguyen/E_commercePC/configuration/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,37 @@
+package com.dainguyen.E_commercePC.configuration;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import org.springframework.http.MediaType;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+import com.dainguyen.E_commercePC.dto.response.ApiResponse;
+import com.dainguyen.E_commercePC.exception.ErrorCode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+    @Override
+    public void commence(
+            HttpServletRequest request, HttpServletResponse response, AuthenticationException authException)
+            throws IOException, ServletException {
+        ErrorCode errorCode = ErrorCode.UNCATEGORIZED_EXCEPTION;
+
+        response.setStatus(errorCode.getHttpStatus().value());
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+
+        ApiResponse<?> apiResponse = ApiResponse.builder()
+                .code(errorCode.getCode())
+                .message(errorCode.getMessage())
+                .build();
+
+        ObjectMapper mapper = new ObjectMapper();
+
+        response.getWriter().write(mapper.writeValueAsString(apiResponse));
+        response.flushBuffer();
+    }
+}
