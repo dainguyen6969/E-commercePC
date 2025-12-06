9130ef1df9e7bbb7717da63536b0265999db7677
dainguyen6969 <daintth03468@fpt.edu.vn>
2025-12-07 06:59:58 +0700

    feat(): add role response.

--- src/main/java/com/dainguyen/E_commercePC/repository/UserRepository.java
+++ src/main/java/com/dainguyen/E_commercePC/repository/UserRepository.java
@@ -3,11 +3,13 @@ package com.dainguyen.E_commercePC.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.dainguyen.E_commercePC.entity.user.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
+    @Query("SELECT DISTINCT u FROM User u JOIN FETCH u.roles WHERE u.username = :username")
     Optional<User> findByUsername(String username);
 }
